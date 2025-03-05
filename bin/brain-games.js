844b1c5a9ebf102a739174c717ad69cfaea90b28
Stanislav-Viktorovich <stimyl89@mail.ru>
2025-03-05 11:49:46 +0300

    add name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,4 @@
-console.log('Welcome to the Brain Games!');
+
+import readlineSync from 'readline-sync';
+const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name?');
+console.log(`Hello, ${name}`);
